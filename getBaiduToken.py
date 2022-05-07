23c3b980f042049a57aa0ff92224597d30480eb0
solar-z <1552443053@qq.com>
2022-05-08 01:45:33 +0800

    add“

--- getBaiduToken.py
+++ getBaiduToken.py
@@ -11,3 +11,17 @@ with open('./_config.yml','+r') as f:
     f.write(t)
 
     f.truncate()
+
+GitalkToken = os.environ["GITALK_TOKEN"]
+GitalkId = os.environ["GITALKID"]
+with open('./_config.butterfly.yml','+r') as f:
+    t = f.read()
+    t = t.replace('${{ secrets.GITALK_TOKEN }}', GitalkToken)
+    t = t.replace('${{ secrets.GITALKID }}', GitalkId)
+
+    print(t)
+
+    f.seek(0, 0)
+    f.write(t)
+
+    f.truncate()
